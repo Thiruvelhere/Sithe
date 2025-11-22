1fb2c163455a53a5d4c9a81051f8ff2964705e17
Thiruvelhere <thiruvel619@gmail.com>
2025-11-22 20:45:31 +0530

    feat: add postinstall script for beta conversion (v1.0.7)

---
+++ scripts/postinstall.js
@@ -0,0 +1,17 @@
+const RESET = "\x1b[0m";
+const BRIGHT = "\x1b[1m";
+const GREEN = "\x1b[32m";
+const CYAN = "\x1b[36m";
+const YELLOW = "\x1b[33m";
+const MAGENTA = "\x1b[35m";
+
+console.log('\n');
+console.log(MAGENTA + "⚡ Thanks for installing Zypher SDK!" + RESET);
+console.log(CYAN + "   The standard for verifiable AI." + RESET);
+console.log('\n');
+console.log(BRIGHT + "🚀 Join the Private Beta & Get $100 Credits:" + RESET);
+console.log(GREEN + "   👉 https://thiruvelhere.github.io/Sithe/" + RESET);
+console.log('\n');
+console.log(YELLOW + "💬 Need help? Join our Discord:" + RESET);
+console.log(CYAN + "   https://discord.gg/vffYw3cGhc" + RESET);
+console.log('\n');
